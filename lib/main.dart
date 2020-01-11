e0c85158e663cbfc7f9aa82442dde8cb038f5e17
darcipons <darcimarie33@gmail.com>
2020-01-10 20:04:25 -0500

    created tabs with different pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: ToDo(),
-      ),
-    );
+void main() => runApp(MyApp());
 
-class ToDo extends StatelessWidget {
-  final List<Tab> myTabs = <Tab>[
-    Tab(text: 'TODO'),
-    Tab(text: 'COMPLETED'),
-  ];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: myTabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Todo App'),
-          backgroundColor: Colors.purple[600],
-          bottom: TabBar(
-            labelColor: Colors.purple[200],
-            unselectedLabelColor: Colors.white,
-            indicatorColor: Colors.purple[200],
-            tabs: myTabs,
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Todo App'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Text('TODO'),
+                Text('COMPLETED'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[Text('Todo'), Text('Completed')],
           ),
         ),
       ),
