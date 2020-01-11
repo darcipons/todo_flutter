aa0449102d81bcf2122c70565c5798499319b4ac
darcipons <darcimarie33@gmail.com>
2020-01-11 18:42:45 -0500

    added todos with checkboxes

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -27,22 +27,52 @@ class _ToDoScreenState extends State<ToDoScreen> {
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.purple,
           onPressed: () {
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter Todo',
-                prefixIcon: IconButton(
-                  icon: Icon(Icons.check_box_outline_blank),
-                  onPressed: () {
-                    setState(() {});
-                    print('Icon button');
-                  },
-                ),
-              ),
-            );
+            print('fab working');
           },
           child: Icon(Icons.add),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text('Setup Flutter environment'),
+              leading: Checkbox(
+                value: false,
+              ),
+            ),
+            ListTile(
+              title: Text('Create git repo'),
+              leading: Checkbox(
+                value: false,
+              ),
+            ),
+            ListTile(
+              title: Text('Create Flutter base project'),
+              leading: Checkbox(
+                value: false,
+              ),
+            ),
+            ListTile(
+              title: Text('Build todo app'),
+              leading: Checkbox(
+                value: false,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+//TextField(
+//decoration: InputDecoration(
+//hintText: 'Enter Todo',
+//prefixIcon: IconButton(
+//icon: Icon(Icons.check_box_outline_blank),
+//onPressed: () {
+//setState(() {});
+//print('Icon button');
+//},
+//),
+//),
+//);
