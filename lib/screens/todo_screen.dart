cdcb189385ead89ed6027551564e84c85b2c8ba9
darcipons <darcimarie33@gmail.com>
2020-01-12 10:51:00 -0500

    created two list items on screen

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -3,8 +3,38 @@ import 'package:flutter/material.dart';
 class TodoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('todo '),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            child: TodoList(),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class TodoList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        TodoTile(),
+        TodoTile(),
+      ],
+    );
+  }
+}
+
+class TodoTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('Setup Flutter environment'),
+      leading: Checkbox(
+        value: false,
+      ),
     );
   }
 }
