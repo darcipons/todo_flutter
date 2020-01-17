ca944d15ed863c2dacbaffc69f88927af64f2da2
darcipons <darcimarie33@gmail.com>
2020-01-16 20:18:03 -0500

    add todo text fields

--- lib/screens/add_todo.dart
+++ lib/screens/add_todo.dart
@@ -3,8 +3,47 @@ import 'package:flutter/material.dart';
 class AddTodo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Second page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Item'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Task Name',
+              labelStyle: TextStyle(color: Colors.purple),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.purple,
+                ),
+              ),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Description',
+              labelStyle: TextStyle(color: Colors.purple),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.purple,
+                ),
+              ),
+            ),
+          ),
+          RaisedButton(
+            child: Text(
+              'Save',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.purple,
+            onPressed: () {
+              print('add item button');
+            },
+          )
+        ],
+      ),
     );
   }
 }
