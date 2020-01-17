22493e32e82a2767a45b15e2d645771d6402fa3f
darcipons <darcimarie33@gmail.com>
2020-01-17 14:56:46 -0500

    added date picker

--- lib/screens/add_todo.dart
+++ lib/screens/add_todo.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:todo_taxfyle/widgets/date_time.dart';
 
-class AddTodo extends StatelessWidget {
+class AddTodo extends StatefulWidget {
+  @override
+  _AddTodoState createState() => _AddTodoState();
+}
+
+class _AddTodoState extends State<AddTodo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,41 +14,49 @@ class AddTodo extends StatelessWidget {
         title: Text('New Item'),
         backgroundColor: Colors.purple,
       ),
-      body: Column(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Task Name',
-              labelStyle: TextStyle(color: Colors.purple),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.purple,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Task Name',
+                labelStyle: TextStyle(color: Colors.purple),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.purple,
+                  ),
                 ),
               ),
             ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Description',
-              labelStyle: TextStyle(color: Colors.purple),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.purple,
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Description',
+                labelStyle: TextStyle(color: Colors.purple),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.purple,
+                  ),
                 ),
               ),
             ),
-          ),
-          RaisedButton(
-            child: Text(
-              'Save',
-              style: TextStyle(color: Colors.white),
+            Row(
+              children: <Widget>[
+                DueDate(),
+              ],
             ),
-            color: Colors.purple,
-            onPressed: () {
-              print('add item button');
-            },
-          )
-        ],
+            RaisedButton(
+              child: Text(
+                'Save',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.purple,
+              onPressed: () {
+                print('add item button');
+              },
+            )
+          ],
+        ),
       ),
     );
   }
