26cd577163cbe0af55350b8f24379dc21f0b7ca8
darcipons <darcimarie33@gmail.com>
2020-01-19 15:59:25 -0500

    tried to get date to show on todo screen

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -1,35 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:todo_taxfyle/widgets/date_time.dart';
 
 class TodoTile extends StatelessWidget {
   final bool isChecked;
   final String todoTitle;
   final String todoDescription;
-//  final String todoDate;
+  final String todoDate;
   final Function checkboxCallback;
 
   TodoTile({
     this.isChecked,
     this.todoTitle,
     this.todoDescription,
-//    this.todoDate,
+    this.todoDate,
     this.checkboxCallback,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        todoTitle,
-        style: TextStyle(
-            decoration: isChecked ? TextDecoration.lineThrough : null),
-      ),
-      subtitle: Text(todoDescription),
-      isThreeLine: true,
-      leading: Checkbox(
-        activeColor: Colors.purple[300],
-        value: isChecked,
-        onChanged: checkboxCallback,
-      ),
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(
+            todoTitle,
+            style: TextStyle(
+                decoration: isChecked ? TextDecoration.lineThrough : null),
+          ),
+          subtitle: Text(todoDescription),
+          isThreeLine: true,
+          leading: Checkbox(
+            activeColor: Colors.purple[300],
+            value: isChecked,
+            onChanged: checkboxCallback,
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            Container(
+              child: DueDate(),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
